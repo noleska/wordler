56e6560fb3554de6c82a7bad6a6ffc9e6a388dc8
noleska <83671681+noleska@users.noreply.github.com>
2024-02-14 22:41:25 -0500

    Update wordler.py
    code analysis compliance, documentation.

--- wordler.py
+++ wordler.py
@@ -7,54 +7,67 @@ Created on Wed Feb 14 17:52:21 2024
 
 import string
 
-word_length = 5                               # wordle words are 5 characters long
-word_file_location = "./answers.txt"          # location of list of possible answer words
-exclude_ends_with_s = True                    # wordle answers are never plural
-vowels_are_worthless = True                   # assign zero score to vowels?
-exclude_words_with_duplicate_letters = True   # exclude from results list words with multiple of the same letter?
-found_in_place_weight = 1.0                   # in scoring words, score weight for letters found in-place
+WORD_LENGTH = 5                               # wordle words are 5 characters long
+# location of list of possible answer words
+WORD_FILE_LOCATION = "./answers.txt"
+EXCLUDE_ENDS_WITH_S = True                    # wordle answers are never plural
+VOWELS_ARE_WORTHLESS = True                   # assign zero score to vowels?
+# exclude from results list words with multiple of the same letter?
+EXCLUDE_WORDS_WITH_DUPLICATE_LETTERS = True
+# in scoring words, score weight for letters found in-place
+FOUND_IN_PLACE_WEIGHT = 1.0
 
-vowels = ['a','e','i','o','u','y']
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
-letter_weights = {'a' : 1.0,
-                  'b' : 1.0,
-                  'c' : 1.0,
-                  'd' : 1.0,
-                  'e' : 1.0,
-                  'f' : 1.0,
-                  'g' : 1.0,
-                  'h' : 1.0,
-                  'i' : 1.0,
-                  'j' : 1.0,
-                  'k' : 1.0,
-                  'l' : 1.0,
-                  'm' : 1.0,
-                  'n' : 1.0,
-                  'o' : 1.0,
-                  'p' : 1.0,
-                  'q' : 1.0,
-                  'r' : 1.0,
-                  's' : 1.0,
-                  't' : 1.0,
-                  'u' : 1.0,
-                  'v' : 1.0,
-                  'w' : 1.0,
-                  'x' : 1.0,
-                  'y' : 1.0,
-                  'z' : 1.0}
+letter_weights = {'a': 1.0,
+                  'b': 1.0,
+                  'c': 1.0,
+                  'd': 1.0,
+                  'e': 1.0,
+                  'f': 1.0,
+                  'g': 1.0,
+                  'h': 1.0,
+                  'i': 1.0,
+                  'j': 1.0,
+                  'k': 1.0,
+                  'l': 1.0,
+                  'm': 1.0,
+                  'n': 1.0,
+                  'o': 1.0,
+                  'p': 1.0,
+                  'q': 1.0,
+                  'r': 1.0,
+                  's': 1.0,
+                  't': 1.0,
+                  'u': 1.0,
+                  'v': 1.0,
+                  'w': 1.0,
+                  'x': 1.0,
+                  'y': 1.0,
+                  'z': 1.0}
 
 banned_characters = [".", ",", "-", "'", " ", r'/', '3']
 
 
 def valid_words_of_length():
+    '''
+    Processes and checks words in word-per-line text document
+    for compliance with Wordle and configuration variables.
+
+    Returns
+    -------
+    out : LIST
+        List of lowercase alpha-only 'valid' words.
+
+    '''
     out = []
-    with open(word_file_location) as word_file:
+    with open(WORD_FILE_LOCATION) as word_file:
         for _word in word_file:
             _word = _word.strip()
-            valid = True;
-            if exclude_ends_with_s and _word.endswith('s'):
+            valid = True
+            if EXCLUDE_ENDS_WITH_S and _word.endswith('s'):
                 valid = False
-            if len(_word) != word_length:
+            if len(_word) != WORD_LENGTH:
                 valid = False
             for _banned_char in banned_characters:
                 if _banned_char in _word:
@@ -66,13 +79,29 @@ def valid_words_of_length():
 
 
 def letter_values(word_list):
+    '''
+    Creates dictionary of numeric scores for alphabet letters.
+    Score is based on how many times the letter appears in the 
+    word list.
+
+    Parameters
+    ----------
+    word_list : LIST
+        List of individual lowercase words.
+
+    Returns
+    -------
+    out : DICTIONARY
+        Keys: Individual lowercase words.
+        Values: Numeric scores of those words
+
+    '''
     out = {}
     for _letter in string.ascii_lowercase:
         out[_letter] = 0
-        
     for _word in word_list:
         for _character in _word:
-            if vowels_are_worthless:
+            if VOWELS_ARE_WORTHLESS:
                 if _character in vowels:
                     continue
             out[_character] += 1 * letter_weights[_character]
@@ -80,6 +109,22 @@ def letter_values(word_list):
 
 
 def has_duplicate_letters(word):
+    '''
+    Returns true if a word contains more than one instance of any letter.
+
+    Parameters
+    ----------
+    word : STRING
+        A single word.
+
+    Returns
+    -------
+    bool
+        True if there is any letter that appears more than once in the input 
+        string.
+        False otherwise.
+
+    '''
     for _char in word:
         count = 0
         for __char in word:
@@ -90,38 +135,73 @@ def has_duplicate_letters(word):
     return False
 
 
-def word_scores(list_words, dict_letter_values):
+def word_scores(list_words):
+    '''
+    Generates dictionary of word scores. Words' scores are based on prevalence
+    of their letters in the input list, as well as configuration options and
+    weights.
+
+    Parameters
+    ----------
+    list_words : LIST
+        List of individual lowercase words.
+
+    Returns
+    -------
+    out : DICTIONARY
+        Keys: individual lowercase words.
+        Values: score numbers.
+
+    '''
     out = {}
     for word in list_words:
-        if exclude_words_with_duplicate_letters and has_duplicate_letters(word):
+        if EXCLUDE_WORDS_WITH_DUPLICATE_LETTERS and has_duplicate_letters(word):
             continue
         word_score = 0
-        charpos1 = 0
-        for char in word:  
-            charpos1 += 1
+        character_index_1 = 0
+        for char in word:
+            character_index_1 += 1
             for _word in list_words:
-                charpos2 = 0
+                character_index_2 = 0
                 for _char in _word:
-                    charpos2 += 1
+                    character_index_2 += 1
                     if char == _char:
-                        if (vowels_are_worthless and (char in vowels)) == False:
-                            if charpos1 == charpos2:
-                                word_score += 1 * letter_weights[char] * found_in_place_weight
+                        if (VOWELS_ARE_WORTHLESS and (char in vowels)) == False:
+                            if character_index_1 == character_index_2:
+                                word_score += 1 * \
+                                    letter_weights[char] * \
+                                    FOUND_IN_PLACE_WEIGHT
                             else:
                                 word_score += 1 * letter_weights[char]
         out[word] = word_score
     return out
 
 
-def sorted_scored_words(dict_word_scores):
-    return sorted(dict_word_scores.items(), key=lambda kv: kv[1], reverse = True)
+def sorted_scored_words(dict_x):
+    '''
+    Sorts input dict by VALUEs, descending.
+
+    Parameters
+    ----------
+    dict_word_scores : DICTIONARY
+        Keys: individual lowercase words
+        Values: numeric word scores
+
+    Returns
+    -------
+    DICTIONARY
+        Sorted by values, in descending order.
+        Keys: individual lowercase words.
+        Values: numeric word scores
+
+    '''
+    return sorted(dict_x.items(), key=lambda kv: kv[1], reverse=True)
 
 
-                    
 if __name__ == "__main__":
     words = valid_words_of_length()
-    dict_letter_values = letter_values(words)
-    dict_word_scores = word_scores(words, dict_letter_values)
+    #dict_letter_values = letter_values(words)
+    dict_word_scores = word_scores(words)
     results = sorted_scored_words(dict_word_scores)
-    for i in range(0,10):
+    for i in range(0, 10):
         print(results[i])
